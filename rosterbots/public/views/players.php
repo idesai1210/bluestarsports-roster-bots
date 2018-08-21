3ff0d983259846bf7cdfa837d841289876e815f6
Ishan Desai <idesai1210@gmail.com>
2018-08-21 00:21:22 -0500

    Add changes to the UI - adding sorting and searching to the table

--- rosterbots/public/views/players.php
+++ rosterbots/public/views/players.php
@@ -10,11 +10,11 @@
             <br>
             <div>
                 <button type="button" ng-click="createOne(team.teamId)" class="btn btn-info" ng-disabled="enable=='false'">
-                    <span ng-show="createOnePlayer == 'Creating...'"><i class="far fa-sync-alt spinning"></i></span>
+                    <span ng-show="createOnePlayer == 'Creating...'"><i class="glyphicon glyphicon-refresh spinning"></i></span>
                     {{ createOnePlayer }}
                 </button>
                 <button type="button" ng-click="fill(team.teamId)" class="btn btn-info" ng-disabled="enable1=='false'">
-                    <span ng-show="fillLineUp == 'Filling Team Sheet...'"><i class="far fa-sync-alt spinning"></i></span>
+                    <span ng-show="fillLineUp == 'Filling Team Sheet...'"><i class="glyphicon glyphicon-refresh spinning"></i></span>
                     {{ fillLineUp }}
                 </button>
             </div>
