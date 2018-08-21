c49160e7be80654602475f5a1f8d296ccb89e7a1
Ishan Desai <idesai1210@gmail.com>
2018-08-20 20:41:28 -0500

    PhpUnit Test Cases

--- rosterbots/tests/TestCase.php
+++ rosterbots/tests/TestCase.php
@@ -5,7 +5,7 @@ namespace Tests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
 
-abstract class TestCase extends BaseTestCase
+class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     public function createApplication()
